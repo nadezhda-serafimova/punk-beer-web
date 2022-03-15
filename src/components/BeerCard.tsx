d939b583cb207a0764c2f919749fb108913aeafb
nadezhda <nadezhda@devlabs.bg>
2022-03-15 23:39:00 +0200

    Change: Add to favs btn state

--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -24,39 +24,46 @@ const beerSound = new Audio('/assets/beer-opening.mp3');
 
 const BeerCard = ({ beer, isHome }: { beer: BeerProps, isHome?: boolean }) => {
   const { id, name, description, image_url } = beer;
-  const { addFav } = useAPI();
+  const { addFav, favourites } = useAPI();
 
   const debouncePlayingBeerSound = _debounce(() => beerSound.play(), 300);
 
+  const isInFavourites = (id: number) => !!favourites.find((fav: any) => fav.id === id);
+
   return (
-      <Card sx={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-        <Box>
-          <Button sx={{ width: 150, height: 150 }} onClick={debouncePlayingBeerSound}>
-            <Img alt={name} src={image_url} />
-          </Button>
-        </Box>
-        <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-          <CardContent sx={{ flex: '1 0 auto' }}>
-            <Typography gutterBottom variant='h5' component='h2'>
-              {name}
-            </Typography>
-            <Typography>
-              {description.length > 200 ? description.substring(0, 200) + '...' : description}
-            </Typography>
-          </CardContent>
-          {
-            isHome && (
-              <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-                <CardActions>
-                  <Button variant='outlined' startIcon={<StarIcon />} onClick={() => addFav(beer)}>
-                    Add to favourites
-                  </Button>
-                </CardActions>
-              </Box>
-            )
-          }
-        </Box>
-      </Card>
+    <Card sx={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+      <Box>
+        <Button sx={{ width: 150, height: 150 }} onClick={debouncePlayingBeerSound}>
+          <Img alt={name} src={image_url} />
+        </Button>
+      </Box>
+      <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography gutterBottom variant='h5' component='h2'>
+            {name}
+          </Typography>
+          <Typography>
+            {description.length > 200 ? description.substring(0, 200) + '...' : description}
+          </Typography>
+        </CardContent>
+        {
+          isHome && (
+            <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+              <CardActions>
+                <Button
+                  variant='outlined'
+                  startIcon={<StarIcon />}
+                  onClick={() => addFav(beer)}
+                  disabled={isInFavourites(id)}
+                >
+                  {isInFavourites(id) ? 'Favourite' : 'Add  to favourites'}
+                </Button>
+              </CardActions>
+            </Box>
+          )
+        }
+      </Box>
+    </Card>
   );
 };
 
--- src/components/BeersListing.tsx
+++ src/components/BeersListing.tsx
@@ -5,7 +5,7 @@ import { BeerProps } from 'types';
 
 const BeersListing = ({ beers, isHome }: { beers: BeerProps[], isHome?: boolean }) => {
   return (
-    <Grid container spacing={4} justifyContent='flex-start' alignItems='stretch'>
+    <Grid container spacing={4} justifyContent='flex-start' alignItems='stretch' sx={{ mb: '60px' }}>
       {beers.map((beer: BeerProps) => (
         <Grid item key={beer.id} sm={6} md={4}>
           <BeerCard beer={beer} isHome={isHome} />
